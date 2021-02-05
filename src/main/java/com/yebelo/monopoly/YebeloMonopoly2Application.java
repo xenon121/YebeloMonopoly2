ae6e958ae96b789b66b9a109c0f4f750f393ff46
Sandeep <sandeep@minebrat.com>
2021-02-05 12:58:48 +0530

    initial commit

---
+++ src/main/java/com/yebelo/monopoly/YebeloMonopoly2Application.java
@@ -0,0 +1,13 @@
+package com.yebelo.monopoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YebeloMonopoly2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YebeloMonopoly2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yebelo/monopoly/YebeloMonopoly2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yebelo.monopoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YebeloMonopoly2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
